e7533b146dd680eb95fc1d659d5e03491e5cdc36
CasaN0va-bhavit <83268213+CasaN0va-bhavit@users.noreply.github.com>
2023-06-27 10:51:33 +0530

    Modiefied the homepage

--- app/page.js
+++ app/page.js
@@ -13,9 +13,11 @@ export default function Home() {
           <p className={styles.homePara}>
             Immerse yourself in a world where dreams come alive, adventures abound, and lifelong memories are waiting to be made. With ParkPass, get ready to unlock the full potential of your theme park visit and embark on a journey filled with excitement, wonder, and unparalleled fun.
           </p>
-        <img className={styles.image1} src="https://media.discordapp.net/attachments/1122590461937328290/1123098958898667550/image.png?width=323&height=472" />
-        <img className={styles.image2} src="https://media.discordapp.net/attachments/1122590461937328290/1123098958898667550/image.png?width=323&height=472" />
-        <img className={styles.image3} src="https://media.discordapp.net/attachments/1122590461937328290/1123098958898667550/image.png?width=323&height=472" />
+          <div className={styles.images}>
+            <img className={styles.image3} src="https://media.discordapp.net/attachments/1122590461937328290/1123115225156173854/Card-1.png?width=429&height=473" />
+            <img className={styles.image2} src="https://media.discordapp.net/attachments/1122590461937328290/1123115225445572668/Card-2.png?width=323&height=472" />
+            <img className={styles.image1} src="https://media.discordapp.net/attachments/1122590461937328290/1123115224866758686/Card.png?width=429&height=473" />
+          </div>
       </div>
     </div>
   )
