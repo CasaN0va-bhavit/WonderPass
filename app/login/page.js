9fa6b9c8fc09acec274c4634c2b3d372084995bc
CasaN0va-bhavit <83268213+CasaN0va-bhavit@users.noreply.github.com>
2023-06-29 15:59:13 +0530

    asxasasadasdada

--- app/login/page.js
+++ app/login/page.js
@@ -2,12 +2,20 @@
 import React, {useState} from 'react'
 import Navbar from '@/components/Navbar';
 import styles from './login.module.css'
+import GoogleLogin from 'react-google-login';
+import jwtdecode from 'jwt-decode'
 
 export default function Login() {
+  const handlefailure = (result) =>{
+    console.log("Failure")
+  }
+  const handlelogin = (googleData) =>{
+    console.log("Successful")
+  }
   return (
     <div>
     <Navbar className={styles.nav} />
-    <form className={styles.screen}>
+    {/* <form className={styles.screen}>
     <div className={styles.background}>
         <div className={styles.shape}></div>
         <div className={styles.shape}></div>
@@ -21,7 +29,15 @@ export default function Login() {
           <a className={styles.loginBtn}><button>Login</button></a>
           <label className={styles.labels}>Don't have an account, <a href="/signup" style={{cursor: "pointer"}}>Sign Up</a></label>
       </div>
-    </form>
+    </form> */}
+
+    
+    <GoogleLogin clientId="1022817115795-n5nihci5rfi3gs1j2m4unumra62ju5jb.apps.googleusercontent.com"
+    buttonText='Log in with Google'
+    onSuccess={handlelogin}
+    onFailure={handlefailure}
+    cookiePolicy={'single_host_origin'}
+    />
     </div>
   )
 }
--- app/plans/page.js
+++ app/plans/page.js
@@ -1,32 +1,55 @@
+"use client"
 import styles from "../home.module.css"
 import Navbar from "@/components/Navbar"
 import mainstyle from "./plans.module.css"
 import doosraStyles from "../about/about.module.css"
+import Header from "./Header"
 export default function Home() {
+
+  // export plan;
+
   return (
     <div>
       <Navbar state="plans"/>
 
-      <div className={mainstyle.content}>
-          <div className={mainstyle.dot1} />
-          <p className={mainstyle.para}>Choose a plan</p>
-          <div className={mainstyle.line} />
-          <h1 className={styles.homeHeading} style={{fontSize: "3.5vw", display: "flex", alignItems: "center", marginTop: "3vh"}}>
-            <span className={styles.gradient1}>PLANS</span>
-          </h1>
-      </div>
+      <Header state="1" />
+      <h1 className={styles.homeHeading} style={{fontSize: "3.5vw", display: "flex", alignItems: "center", marginTop: "3vh"}}>
+      <span className={styles.gradient1}>PLANS</span>
+    </h1>
       <div className={mainstyle.plans}>
         <div className={mainstyle.card} id={mainstyle.single}>
           <h1 className={mainstyle.cardHeading}>Single</h1>
-          <a className={mainstyle.cardButton}><h1 className={mainstyle.btnText}>Buy Now</h1></a>
-        </div>
+          <ul className={mainstyle.ul}>
+            <li className={mainstyle.li}><p className={mainstyle.benefitsPara}>Scanning</p></li>
+            <li className={mainstyle.li}><p className={mainstyle.benefitsPara} style={{marginTop: "2.5vw", listStyle: "disc"}}>Seat Selection</p></li>
+            <li className={mainstyle.li}><p className={mainstyle.benefitsPara} style={{marginTop: "5vw", listStyle: "disc"}}>Video Access</p></li>
+          </ul>
+          <p className={mainstyle.paraMoney}>$85</p>
+          <a className={mainstyle.cardButton} ><span className={mainstyle.btnText}>Choose</span></a>
+       </div>
         <div className={mainstyle.card} id={mainstyle.family}>
           <h1 className={mainstyle.cardHeading}>Family</h1>
-          <a className={mainstyle.cardButton}><h1 className={mainstyle.btnText}>Buy Now</h1></a>
+          <p className={mainstyle.para1}>Most Popular</p>
+          <ul className={mainstyle.ul}>
+            <li className={mainstyle.li}><p className={mainstyle.benefitsPara}>Scanning</p></li>
+            <li className={mainstyle.li}><p className={mainstyle.benefitsPara} style={{marginTop: "2.5vw", listStyle: "disc"}}>Seat Selection</p></li>
+            <li className={mainstyle.li}><p className={mainstyle.benefitsPara} style={{marginTop: "5vw", listStyle: "disc"}}>Video Access</p></li>
+            <li className={mainstyle.li}><p className={mainstyle.benefitsPara} style={{marginTop: "7.5vw", listStyle: "disc"}}>Dynamic Itinery System</p></li>
+            <li className={mainstyle.li}><p className={mainstyle.benefitsPara} style={{marginTop: "10vw", listStyle: "disc"}}>Rent Golf Cart for Free</p></li>
+          </ul>
+          <p className={mainstyle.paraMoney} style={{top: "9.5vw"}}>$150</p>
+          <a className={mainstyle.cardButton} ><span className={mainstyle.btnText}>Choose</span></a>
         </div>
         <div className={mainstyle.card} id={mainstyle.duo}>
           <h1 className={mainstyle.cardHeading}>Duo</h1>
-          <a className={mainstyle.cardButton}><h1 className={mainstyle.btnText}>Buy Now</h1></a>
+          <ul className={mainstyle.ul}>
+            <li className={mainstyle.li}><p className={mainstyle.benefitsPara}>Scanning</p></li>
+            <li className={mainstyle.li}><p className={mainstyle.benefitsPara} style={{marginTop: "2.5vw", listStyle: "disc"}}>Seat Selection</p></li>
+            <li className={mainstyle.li}><p className={mainstyle.benefitsPara} style={{marginTop: "5vw", listStyle: "disc"}}>Video Access</p></li>
+            <li className={mainstyle.li}><p className={mainstyle.benefitsPara} style={{marginTop: "7.5vw", listStyle: "disc"}}>Dynamic Itinery System</p></li>
+          </ul>
+          <p className={mainstyle.paraMoney}>$100</p>
+          <a className={mainstyle.cardButton} ><span className={mainstyle.btnText}>Choose</span></a>
         </div>
       </div>
     </div>
