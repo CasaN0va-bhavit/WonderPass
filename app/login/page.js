fc36226e723abc70b4c3bde67eb17cafd519d724
CasaN0va-bhavit <83268213+CasaN0va-bhavit@users.noreply.github.com>
2023-06-30 10:58:50 +0530

    done

--- app/login/page.js
+++ app/login/page.js
@@ -1,41 +1,48 @@
 "use client"
-import React, {useState} from 'react'
+import React, {useRef} from 'react'
 import Navbar from '@/components/Navbar';
 import styles from './login.module.css'
+// import GoogleLogin from 'react-google-login';
+import home from "../home.module.css"
+import emailjs from '@emailjs/browser';
+import Header from "../plans/Header"
+
 
 export default function Login() {
-  const handlefailure = (result) =>{
-    console.log("Failure")
-  }
-  const handlelogin = (googleData) =>{
-    console.log("Successful")
-  }
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_mdh9nrd', 'template_aivsxdl', form.current, 'lxiBQPKbK_FiMS41l')
+      .then((result) => {
+          console.log(result.text);
+          alert("Thanks for your purchase. You've received an email of your QR code.")
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
+
+  
   return (
     <div>
     <Navbar className={styles.nav} />
-    {/* <form className={styles.screen}>
-    <div className={styles.background}>
-        <div className={styles.shape}></div>
-        <div className={styles.shape}></div>
+    <Header state="2"/>
+    <h1 className={home.homeHeading} style={{textAlign:"center",marginTop:"0vh"}}>
+      
+      <span className={home.gradient1}>LOG IN</span> </h1>
+    {/* <div className={styles.center}>
+    <div id="signindiv" className={styles.finclass}>
     </div>
-      <div className={styles.loginBox}>
-          <h1 className={styles.loginHead}>Login</h1>
-          <label className={styles.labels}>Username</label>
-          <input className={styles.username} type='text' placeholder='Username' />
-          <label className={styles.labels}>Password</label>
-          <input className={styles.password} type='password' placeholder='Password' />
-          <a className={styles.loginBtn}><button>Login</button></a>
-          <label className={styles.labels}>Don't have an account, <a href="/signup" style={{cursor: "pointer"}}>Sign Up</a></label>
-      </div>
-    </form> */}
+    </div>
+      <button className={styles.cardButton}><span className="btnText">Mail</span></button>
+      <script src="https://smtpjs.com/v3/smtp.js"></script> */}
+      <form ref={form} className={styles.loginBox} onSubmit={sendEmail} >
+        <label className={styles.labels}>Email</label>
+        <input type='email' className={styles.username} name="user_emai" />
+        <button type='submit' className={styles.loginBtn}>Get email</button>
+      </form>
 
-    
-    <GoogleLogin clientId="1022817115795-n5nihci5rfi3gs1j2m4unumra62ju5jb.apps.googleusercontent.com"
-    buttonText='Log in with Google'
-    onSuccess={handlelogin}
-    onFailure={handlefailure}
-    cookiePolicy={'single_host_origin'}
-    />
     </div>
   )
 }
--- components/Navbar.js
+++ components/Navbar.js
@@ -15,7 +15,7 @@ export default function Navbar(props) {
         <h1 className={styles.link} style={props.state == "features" ? {color: "#FFA8DC"} : {color: "#FFF"}}><a href="/features" className="cursor-pointer no-underline">Features</a></h1>
         <h1 className={styles.link} style={props.state == "plans" ? {color: "#FFA8DC"} : {color: "#FFF"}}><a href="/plans" className="cursor-pointer no-underline">Plans</a></h1>
         <h1 className={styles.link} style={props.state == "reviews" ? {color: "#FFA8DC"} : {color: "#FFF"}}><a href="/reviews" className="cursor-pointer no-underline">Reviews</a></h1>
-        <h1 className={styles.link} style={props.state == "login" ? {color: "#FFA8DC"} : {color: "#FFF"}}><a href="/login" className="cursor-pointer no-underline">Login</a></h1>
+        {/* <h1 className={styles.link} style={props.state == "login" ? {color: "#FFA8DC"} : {color: "#FFF"}}><a href="/login" className="cursor-pointer no-underline">Login</a></h1> */}
       </div>
     </div>
   )
